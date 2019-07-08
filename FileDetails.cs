e736ed51aa73a0436b7f57a23b27af8a6c7143fb
Alexander Skrinnik <askrinnik@amtoss.com.ua>
2019-07-08 11:47:27 +0300

    Improve code Add a feature to read File extended properties through Shell

---
+++ FileDetails.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Shell32;
+
+namespace FileNameToModifyDate
+{
+  public static class FileDetails
+  {
+    public static Task<T> StartSTATask<T>(Func<T> func)
+    {
+      var tcs = new TaskCompletionSource<T>();
+      var thread = new Thread(() =>
+      {
+        try
+        {
+          tcs.SetResult(func());
+        }
+        catch (Exception e)
+        {
+          tcs.SetException(e);
+        }
+      });
+      thread.SetApartmentState(ApartmentState.STA);
+      thread.Start();
+      return tcs.Task;
+    }
+
+    public static string[] ListAllDetailsForFile(string filePath)
+    {
+      // Work with shell should be in method with [STAThread] attribute or in the Thread with ApartmentState.STA
+      return StartSTATask(() =>
+      {
+        var shell = new ShellClass();
+        var folder = shell.NameSpace(Path.GetDirectoryName(filePath));
+        var folderItem = folder.ParseName(Path.GetFileName(filePath));
+
+        var lines = new List<string>();
+        for (var i = 1; i < 500; i++)
+        {
+          var detailName = folder.GetDetailsOf(null, i).Trim();
+          var detailValue = folder.GetDetailsOf(folderItem, i).Trim();
+          if (!string.IsNullOrWhiteSpace(detailName))
+            lines.Add($"({i}) {detailName}: {detailValue}");
+        }
+        return lines.ToArray();
+      }).Result;
+    }
+
+    public static DateTime GetMediaDateFromFileName(string filePath)
+    {
+      // Work with shell should be in method with [STAThread] attribute or in the Thread with ApartmentState.STA
+      var value = StartSTATask(() =>
+      {
+        var shell = new ShellClass();
+        var folder = shell.NameSpace(Path.GetDirectoryName(filePath));
+        var folderItem = folder.ParseName(Path.GetFileName(filePath));
+
+        const int mediaCreatedValue = 208;
+        return folder.GetDetailsOf(folderItem, mediaCreatedValue).Trim();
+      }).Result;
+
+      value = RemoveInvalidCharacters(value);
+      // If the value string is empty, return DateTime.MinValue, otherwise return the "Media Created" date
+      return value == string.Empty ? DateTime.MinValue : DateTime.Parse(value);
+    }
+
+    private static string RemoveInvalidCharacters(string value)
+    {
+      // These are the characters that are not allowing me to parse into a DateTime
+      var charactersToRemove = new[] {(char) 8206, (char) 8207};
+      // Removing the suspect characters
+      foreach (var c in charactersToRemove)
+        value = value.Replace(c.ToString(), "").Trim();
+      return value;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -6,38 +6,76 @@ namespace FileNameToModifyDate
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
-      var folderName = @"E:\Disk_G\DiskReserv\Video\1998";
-      var fileMask = @"*.avi";
-      var dateMask = @"(?<year>\d{2})\.(?<month>\d{2})\.(?<day>\d{2}).+";
+      //var details = FileDetails.ListAllDetailsForFile(@"D:\Downloads\VID_20190407_095244.mp4");
+      //Console.WriteLine(string.Join("\n", details));
 
-      var filePathes = Directory.GetFiles(folderName, fileMask);
-      foreach (var filePath in filePathes)
-        ProcessFile(filePath, dateMask);
+      //var mediaDate = FileDetails.GetMediaDateFromFileName(@"D:\Downloads\VID_20190407_095244.mp4");
+
+      const string folderName = @"D:\Downloads";
+      const string fileMask = @"*.mp4";
+
+      var filePaths = Directory.GetFiles(folderName, fileMask);
+      Console.WriteLine($"{filePaths.Length} files were detected.");
+
+      foreach (var filePath in filePaths)
+        SetFileDateFromFileName(filePath);
+
       Console.WriteLine("Done!");
+      Console.ReadLine();
     }
 
-    private static void ProcessFile(string filePath, string dateMask)
+    private static void SetFileDateFromFileName(string filePath)
     {
-      var fileName = Path.GetFileName(filePath);
-      var date = GetDateFromText(fileName, dateMask);
-      Console.WriteLine($"Setting date {date} to {fileName}...");
+      var date = GetDateFromFileName(filePath);
+
+      Console.WriteLine($"Setting date {date} to {filePath}...");
+
       File.SetLastWriteTime(filePath, date);
+      //File.SetCreationTime(filePath, date);
+      //File.SetLastAccessTime(filePath, date);
     }
 
-    private static DateTime GetDateFromText(string fileName, string dateMask)
+    private static DateTime GetDateFromFileName(string filePath)
     {
-      var regex = new Regex(dateMask);
-      var m = regex.Match(fileName);
+      var dateMask1 = @"(?<year>\d{2})\.(?<month>\d{2})\.(?<day>\d{2}).+";
+      var dateMask2 = @"(?<year>\d{2})\.(?<month>\d{2}).+";
 
-      var yearS = m.Groups["year"].Value;
-      var month = m.Groups["month"].Value;
-      var day = m.Groups["day"].Value;
+      var fileName = Path.GetFileName(filePath) ?? throw new ArgumentOutOfRangeException(nameof(filePath), "Invalid value: " + filePath);
+
+      var m = new Regex(dateMask1).Match(fileName);
+      if (m.Success)
+      {
+        var year = GetYear(m.Groups["year"].Value);
+
+        var month = m.Groups["month"].Value;
+        var day = m.Groups["day"].Value;
+
+        var date = new DateTime(year, int.Parse(month), int.Parse(day), 10, 0, 0);
+        return date;
+      }
+
+      m = new Regex(dateMask2).Match(fileName);
+      if (m.Success)
+      {
+        var year = GetYear(m.Groups["year"].Value);
+
+        var month = m.Groups["month"].Value;
+        var day = "1";
+
+        var date = new DateTime(year, int.Parse(month), int.Parse(day), 10, 0, 0);
+        return date;
+      }
+      throw new ArgumentOutOfRangeException(nameof(filePath), "Invalid value: " + filePath);
+    }
+
+    private static int GetYear(string yearS)
+    {
       var year = int.Parse(yearS);
-      year += year > 40 ? 1900 : 2000;
-      var date = new DateTime(year, int.Parse(month), int.Parse(day));
-      return date;
+      if (year < 100)
+        year += year > 40 ? 1900 : 2000;
+      return year;
     }
   }
 }
